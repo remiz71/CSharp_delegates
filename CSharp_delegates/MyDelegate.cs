4f956d9c4865224daa690ddde57d2b7f493b9aaa
Dima <jkhdjfjkjshjb@gmail.com>
2022-02-07 15:00:15 +0300

    Add project files.

---
+++ CSharp_delegates/MyDelegate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_delegates
+{
+    internal class StringOperations
+    {
+        
+
+        // Методы
+
+        public string UppercaseStr(string str)
+        {
+            Console.Write("Замена регистра букв строки на верхний регистр: ");
+            
+            return str.ToUpper();
+        }
+
+        public string Lowercasestr(string str)
+        {
+            Console.Write("Замена регистра букв в строке на нижний регистр: ");
+            return str.ToLower();
+            
+        }
+        public string ReplaceSpacers(string str)
+        {
+            Console.Write("Замена пробелов звездочками: ");
+            return str.Replace(' ', '*');
+           
+        }
+        public string DeleteSpacers(string str)
+        {
+            Console.Write("Удаление пробелов: ");
+            string temp = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != ' ')
+                    temp += str[i];
+            }
+            return temp;
+            
+        }
+        
+    }
+}
---
+++ CSharp_delegates/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_delegates
+{
+    public delegate string strFormat(string str);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Это новая строка которую надо отформатировать";
+            Console.WriteLine($"Исходная строка: {str}");
+            StringOperations strOp = new StringOperations();
+            strFormat str1 = strOp.Lowercasestr;
+            strFormat str2 = strOp.DeleteSpacers;
+            strFormat str3 = strOp.UppercaseStr;
+            str3 += strOp.ReplaceSpacers;
+            str3 += strOp.Lowercasestr;
+            str2 += strOp.Lowercasestr;
+            str2 += strOp.UppercaseStr;
+            str1 += strOp.UppercaseStr;
+            str1 += strOp.DeleteSpacers;
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Отработка первого делегата: ");
+            foreach (strFormat format in str1.GetInvocationList())
+            {
+                Console.WriteLine(format(str));
+                Console.WriteLine();
+            }
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Отработка второго делегата: ");
+            foreach (strFormat format in str2.GetInvocationList())
+            {
+                Console.WriteLine(format(str));
+                Console.WriteLine();
+            }
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Отработка третьего делегата: ");
+            foreach (strFormat format in str3.GetInvocationList())
+            {
+                Console.WriteLine(format(str));
+                Console.WriteLine();
+            }
+            Console.WriteLine("--------------------------------------");
+            
+            
+        }
+    }
+}
